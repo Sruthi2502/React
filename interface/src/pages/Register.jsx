1f1ce22914f2eb201a0fcdfa8c52fa075e74dd27
Sruthi2502 <sruthibajjuri04@gmail.com>
2024-11-08 23:50:01 +0530

    Update Register.jsx

--- interface/src/pages/Register.jsx
+++ interface/src/pages/Register.jsx
@@ -144,7 +144,7 @@ const Register = () => {
       try {
         setLoading(true); // Show loading spinner
         const response = await axios.post(
-          "http://localhost:5004/user/register",
+          "https://react-git-main-sruthi-bajjuris-projects.vercel.app/",
           formData
         );
         setSuccessMessage(response.data.message);
